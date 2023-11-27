d88999755fff188c99e429fdcdbb23236fb51d95
Harry Zhou <hzhou51@my.bcit.ca>
2023-11-27 00:28:08 -0800

    Image path function for tile.py

--- components/tile.py
+++ components/tile.py
@@ -1,5 +1,5 @@
-colors = ["red","blue","yellow","black","joker"]
-numbers = ["1","2","3","4","5","6","7","8","9","10","11","12","13","*"]
+colors = ["r","b","y","bl","joker"]
+numbers = ["1","2","3","4","5","6","7","8","9","10","11","12","13"]
 
 class Tile:
     def __init__(self, color, number):
@@ -8,14 +8,19 @@ class Tile:
         else:
             self.color = color
             self.number = number
+            self.path_to_image = self.get_image_path()
     
     def __str__(self):
-        return f"{self.color} {self.number}"
+        return f"{self.color}{self.number}"
     
     def sameNumber(self,other):
         return self.number == other.number
 
     def sameColor(self,other):
         return self.color == other.color
+    
+    def get_image_path(self):
+         pass
+    
     
     
